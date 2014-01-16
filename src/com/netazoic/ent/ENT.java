6913e7a1d4bdabd2e16bfaaaa6426ed4e0b74dd2
John Moore <jmoore@netazoic.com>
2014-01-15 21:36:00 -0800

    Some path refactoring

--- src/com/netazoic/ent/ENT.java
+++ src/com/netazoic/ent/ENT.java
@@ -1,13 +1,21 @@
 package com.netazoic.ent;
 
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.netazoic.util.SQLUtil;
+
 
 
 public class ENT implements IF_Ent{
@@ -16,32 +24,129 @@ public class ENT implements IF_Ent{
 	public String fld_nitID = null;
 	public String sql_RetrieveENT;
 	public String sql_CreateEVT;
+	
+	public String nitName = null;
+	public String nitID = null;
+	public String nitCode = null;
+	public String nitTitle = null;
+	public String nitDesc = null;
+	public String nitURL = null;
+	public String nitTypeCode = null;
+
+	public String nitTable = null;
+
 	@JsonIgnore 
 	public Connection con;
 
+	public static List<Field> getAllFields(List<Field> fields, Class<?> type) {
+		// http://stackoverflow.com/questions/1042798/retrieving-the-inherited-attribute-names-values-using-java-reflection
+		for (Field field: type.getDeclaredFields()) {
+			fields.add(field);
+		}
 
-	public void retrieveRecord() {
+		if (type.getSuperclass() != null) {
+			fields = getAllFields(fields, type.getSuperclass());
+		}
+
+		return fields;
+	}
+
+	public static List<Field> getAllFields(List<Field> fields, Class<?> type,boolean flgInherit) {
+		// http://stackoverflow.com/questions/1042798/retrieving-the-inherited-attribute-names-values-using-java-reflection
+		for (Field field: type.getDeclaredFields()) {
+			fields.add(field);
+		}
+		if(flgInherit){
+			if (type.getSuperclass() != null) {
+				fields = getAllFields(fields, type.getSuperclass());
+			}		
+		}
+
+		return fields;
+	}
+
+
+	public void retrieveRecord() throws Exception {
 		Statement stat = null;
 		try{
 			Object nitIDObj;
 			if(nitIDField == null) 	nitIDField = this.getClass().getField(fld_nitID);
 			nitIDObj = nitIDField.get(this);
 			if(nitIDObj == null) throw new Exception("Must first set record ID value.");
-			String ctp = sql_RetrieveENT;
+			String fPath = sql_RetrieveENT;
 			Map<String, Object> settings = new HashMap<String, Object>();
 			settings.put(nitIDField.getName(), nitIDObj);
-			String sql = SQLUtil.parseQuery(settings, ctp);
+			String sql = SQLUtil.parseQueryFile(settings, fPath);
 			stat = con.createStatement();
 			ResultSet rs = SQLUtil.execSQL(sql,stat);
 			nitID = nitIDObj.toString();
 			setFieldVals(rs);
 			//twiddleWebuserIterator();
 		}catch(Exception ex){
-			throw new ClarescoException(ex);
+			throw ex;
 		}finally{
 			if(stat != null) try{stat.close();stat = null;}catch(Exception ex){}
 		}	
 
+	}
+
+	
+	public void setFieldVals(ResultSet rs) throws SQLException{
+		//load object fields from similarly named db fields
+		/*
+		 * At this level, the setFieldVals function can only set values on Public fields in the 
+		 * extending class. If you wish to work with private or package scope fields, override
+		 * this function with a copy in the local class.
+		 */
+		List<Field> flds= getAllFields(new LinkedList<Field>(),this.getClass());
+		//Field[] flds = this.getClass().getDeclaredFields();
+		Object val;
+		String fld;
+		Class type;
+		rs.next();
+		int idx = 0;
+		ResultSetMetaData rsmd = rs.getMetaData();
+		Map<String,Integer> colMap = new HashMap<String,Integer>();
+		for (int i = 1; i < rsmd.getColumnCount()+1; i++){
+			colMap.put(rsmd.getColumnName(i).toLowerCase(), i);
+		}
+		for(Field f : flds){
+			try{
+				fld = f.getName();
+				if(!colMap.containsKey(fld.toLowerCase()))continue;
+				val = rs.getObject(fld);
+				//if(val==null) continue;
+				type = f.getType();
+				if( type.isInstance(val)){
+					//nada, type is the same between rs and object. No conversion necessary.
+				}
+				else if((type.equals(Integer.class) || (type.equals(int.class))) && ( val instanceof java.math.BigDecimal)){
+					BigDecimal mybd = (BigDecimal)val;
+					val = mybd.intValueExact();					
+				}
+				else if(type.equals(String.class) && val instanceof java.math.BigDecimal){
+					//need to convert to a String
+					val = val.toString();
+				}
+				else if(type.equals(java.sql.Date.class) && val instanceof java.sql.Timestamp){
+					Timestamp ts = (Timestamp)val;
+					val = new java.sql.Date(ts.getTime());
+				}
+				else if(type.equals(java.util.Date.class) && val instanceof java.sql.Timestamp){
+					Timestamp ts = (Timestamp)val;
+					val = new java.util.Date(ts.getTime());
+				}
+				else if (type.equals(String.class) && val instanceof java.sql.Date){
+					//need to convert Date to a String
+					val = val.toString();
+				}
+				f.set(this, val);
+			}catch(Exception ex){
+				@SuppressWarnings("unused")
+				String msg = ex.getMessage();
+				continue;
+			}
+		}
 	}
 
 
--- src/com/netazoic/util/SQLUtil.java
+++ src/com/netazoic/util/SQLUtil.java
@@ -1,5 +1,158 @@
 package com.netazoic.util;
 
-public class SQLUtil {
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map;
+import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+
+
+
+public class SQLUtil {
+	
+	public enum SQL_Param{
+		UserName, DatabaseServer, Password, jdbcLogLevel
+	}
+
+	public static int execSQL(String q, Connection con) throws Exception{
+		Statement myStat = null;
+		int result = 0;
+		try{
+			myStat = con.createStatement();
+			myStat.execute(q);
+			result = myStat.getUpdateCount();
+		}
+		catch(SQLException sqlex){
+			throw new SQLException(sqlex);
+		}
+		catch(Exception ex){
+			throw new Exception(ex);
+		}
+		finally{
+			if(myStat != null)try{myStat.close();myStat=null;}catch(SQLException sqlex){}
+		}
+		return result;
+	}
+
+	public synchronized static String execSQL(String myQuery,String key, Connection con) throws SQLException{
+
+		Statement myStat = null;
+		String result = null;
+		ResultSet rs = null;
+		try{
+			myStat = con.createStatement();
+			rs = myStat.executeQuery(myQuery);
+			if(rs.next()){
+				result = rs.getString(key);
+			}
+		}
+		catch(SQLException sqlex){
+			throw new SQLException(sqlex);
+		}
+		finally{
+			if(myStat != null)try{myStat.close();myStat=null;}catch(SQLException sqlex){}
+		}
+		return result;
+	}
+	
+	  public synchronized static ResultSet execSQL(String myQuery, Statement myStat) throws SQLException{
+
+		    String strVal = null;
+		    ResultSet rs = null;
+		    try{
+		      rs = myStat.executeQuery(myQuery);
+		    }
+		    catch(SQLException ex){
+		      throw ex;
+		    }
+		    finally{
+		      // don't close the statement		    
+		    }
+		    return rs;
+		  }
+		  
+	
+	
+	public static Connection getConnection(String userName, String password, String connURL, String jdbcLogLevel) throws SQLException {
+
+	    Connection conn = null;
+	    try {
+	    	Properties props = new Properties();
+	    	props.setProperty(pg"user", userName);
+	    	props.setProperty("password", password);
+	    	props.setProperty("logUnclosedConnections", "true");
+	    	props.setProperty("logLevel", jdbcLogLevel);	
+	    	//props.setProperty("standard_conforming_strings", "on");
+	    	conn = DriverManager.getConnection(connURL,props);
+	    } catch (SQLException sqlex) {
+	      // TODO Auto-generated catch block
+	      throw sqlex;
+	    }
+	    return conn;
+	}
+	
+	 public static Connection getConnection(Map<String,String>map) throws  SQLException{
+		    Connection conn;
+		    String userName,password,jdbcLogLevel,connURL;
+		    connURL = (String)map.get(SQL_Param.DatabaseServer.name());
+		    userName = (String)map.get(SQL_Param.UserName.name());
+		    password = (String)map.get(SQL_Param.Password.name());
+		    jdbcLogLevel = (String)map.get(SQL_Param.jdbcLogLevel.name());
+		    return getConnection(userName,password,connURL,jdbcLogLevel);
+		  }
+
+	  public static String parseQueryFile(Map<String,Object> settings, String path) throws Exception{
+		String q = readFile(path);
+		return parseQuery(settings, q);
+	}
+
+	public static String parseQuery(Map<String,Object> settings, String q) throws Exception{
+
+		try {
+			String key, val,token;
+			Object valObj;
+			for(Map.Entry<String,Object> entry: settings.entrySet()){
+				key = entry.getKey();
+				valObj = entry.getValue();
+				val = valObj.toString();
+				token = "/$" + key + ";/";
+				q.replaceAll(token, val);
+			}
+		} catch (Exception ex) {
+			throw ex;
+		}
+		return q;
+	}
+
+	static String readFile(String path) throws IOException{
+		Charset charset = StandardCharsets.UTF_8;
+		return readFile(path,charset);
+	}
+
+	static String readFile(String path, Charset encoding) 
+			throws IOException 
+			{
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+			}
+
+	public static String readLargeFile(File file) throws IOException{
+		return FileUtils.readFileToString(file);
+	}
+	
+	public static void releaseConnection(Connection con) throws SQLException{
+		con.close();
+		con = null;
+	}
 }
